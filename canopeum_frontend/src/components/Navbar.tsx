2f6d545c51329893e0f48e72d4ee2150ebc34d89
Theo Auffeuvre <theo.auffeuvre@beslogic.com>
2024-03-18 14:50:26 -0400

    Initial commit

--- canopeum_frontend/src/components/Navbar.tsx
+++ canopeum_frontend/src/components/Navbar.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import iconHome from "@assets/icons/home-regular.svg";
+import iconAnalytics from "@assets/icons/analytics-regular.svg";
+import iconMap from "@assets/icons/pin-map-regular.svg";
+import iconUser from "@assets/icons/user-regular.svg";
 
 export default function Navbar() {
     return (
@@ -23,22 +26,22 @@ export default function Navbar() {
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
                             <Link className="nav-link" to="/home">
-                                Home
+                                <img src={iconHome} alt="iconHome"/>
                             </Link>
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link" to="/analytics">
-                                About
+                                <img src={iconAnalytics} alt="iconAnalytics" />
                             </Link>
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link" to="/map">
-                                Services
+                                <img src={iconMap} alt="iconMap" />
                             </Link>
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link" to="/settings">
-                                Settings
+                                <img src={iconUser} alt="iconUser" />
                             </Link>
                         </li>
                     </ul>
--- canopeum_frontend/src/pages/Analytics.tsx
+++ canopeum_frontend/src/pages/Analytics.tsx
@@ -1,10 +1,40 @@
-import icon from "@assets/icons/account-regular.svg";
+import iconDiploma from "@assets/icons/diploma-regular.svg";
+import iconPinMap from "@assets/icons/pin-map-solid.svg";
 
 export default function Analytics() {
     return (
         <div>
-            <img src={icon} alt="icon" />
             <h1>Analytics</h1>
+            <button type="button" className="btn btn-primary btn-sm">Primary sm</button>
+            <button type="button" className="btn btn-primary">Primary md</button>
+            <button type="button" className="btn btn-primary btn-lg">Primary lg</button>
+
+            <button type="button" className="btn btn-secondary btn-sm">Secondary sm</button>
+            <button type="button" className="btn btn-secondary">Secondary md</button>
+            <button type="button" className="btn btn-secondary btn-lg">Secondary lg</button>
+
+            <button type="button" className="btn btn-outline-primary btn-sm">Primary Outline sm</button>
+            <button type="button" className="btn btn-outline-primary">Primary Outline md</button>
+            <button type="button" className="btn btn-outline-primary btn-lg">Primary Outline lg</button>
+
+            <button type="button" className="btn btn-outline-secondary btn-sm">Secondary Outline sm</button>
+            <button type="button" className="btn btn-outline-secondary">Secondary Outline md</button>
+            <button type="button" className="btn btn-outline-secondary btn-lg">Secondary Outline lg</button>
+
+            <div className="card d-flex flex-row" style={{ width: '30rem'}}>
+                <img src="/Canopeum_Logo.jpg" className="w-25 img-fluid" alt="..."/>
+                <div className="card-body w-75">
+                    <h5 className="card-title">Canopeum Site A</h5>
+                    <p className="card-text">
+                        <img src={iconDiploma} alt="iconDiploma"></img>
+                        Site type X
+                     </p>
+                    <p className="card-text">
+                        <img src={iconPinMap} alt="iconPinMap"></img>
+                        Saint-Denis-sur-Richelieu
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
