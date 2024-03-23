e1cddf88462481a56224557d00c1ab29008f296d
Samuel Poirier <samuel.poirier@beslogic.com>
2024-03-23 10:37:32 -0400

    Applied linting rules to all tsx files Fix typo

--- canopeum_frontend/src/App.tsx
+++ canopeum_frontend/src/App.tsx
@@ -1,31 +1,29 @@
 import './App.scss';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Analytics from './pages/Analytics';
-import Map from './pages/Map';
-import UserManagement from './pages/UserManagement';
-import Login from './pages/Login';
-
-import Navbar from './components/Navbar';
-import Utilities from './pages/Utilities';
-
 import 'bootstrap/js/index.umd.js';
 
-export default function App() {
-    return (
-        <>
-            <BrowserRouter>
-            <Navbar />
-                <Routes>
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/" element={<Home />} />
-                    <Route path="/analytics" element={<Analytics />} />
-                    <Route path="/map" element={<Map />} />
-                    <Route path="/user-management" element={<UserManagement />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/utilities" element={<Utilities />} />
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
-}
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Analytics from './pages/Analytics';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Map from './pages/Map';
+import UserManagement from './pages/UserManagement';
+import Utilities from './pages/Utilities';
+
+const App = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route element={<Home />} path="/home" />
+      <Route element={<Home />} path="/" />
+      <Route element={<Analytics />} path="/analytics" />
+      <Route element={<Map />} path="/map" />
+      <Route element={<UserManagement />} path="/user-management" />
+      <Route element={<Login />} path="/login" />
+      <Route element={<Utilities />} path="/utilities" />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
