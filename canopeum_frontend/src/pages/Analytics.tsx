a3266fcaf026e0547f8a1f511eff6615e4170a66
Nicolas Dontigny <nicolas.dontigny@beslogic.com>
2024-03-23 12:36:02 -0400

    Displaying responsive cards & all sections

--- canopeum_frontend/src/pages/Analytics.tsx
+++ canopeum_frontend/src/pages/Analytics.tsx
@@ -5,20 +5,20 @@ import api from '../services/apiInterface';
 import { ensureError } from '../services/errors';
 
 const Analytics = () => {
-  const [sites, setSites] = useState<SiteSummary[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [siteSummaries, setSiteSummaries] = useState<SiteSummary[]>([]);
+  // TODO(NicolasDontigny): Handle site summaries loading & error
+  const [isLoadingSites, setIsLoadingSites] = useState(false);
   const [error, setError] = useState<Error | undefined>(undefined);
 
   const fetchSites = async () => {
-    setIsLoading(true);
+    setIsLoadingSites(true);
     try {
       const response = await api.sites.summary();
-      console.log('ALL SITES response:', response);
-      setSites(response);
+      setSiteSummaries(response);
     } catch (error_: unknown) {
       setError(ensureError(error_));
     } finally {
-      setIsLoading(false);
+      setIsLoadingSites(false);
     }
   };
 
@@ -27,36 +27,58 @@ const Analytics = () => {
   }, []);
 
   const renderSiteCards = () =>
-    sites.map(site => (
+    siteSummaries.map(site => (
       <div
-        className='col-3'
-        key={site.id}
+        className='col-12 col-md-6 col-lg-3'
+        key={site.name}
       >
-        <div className='card-body d-flex flex-column h-100'>
-          <h5 className='card-title'>{site.name ?? 'Unnamed site'}</h5>
-          <p className='card-text'>
-            Site content here.
-          </p>
+        <div className="card h-100">
+          <div className='card-body d-flex flex-column h-100'>
+            <h5 className='card-title'>{site.name ?? 'Unnamed site'}</h5>
+            <p className='card-text'>
+              Site summary content here.
+            </p>
+          </div>
         </div>
       </div>
     ))
 
-  return (
-  <div>
-    <div className='container pt-3 d-flex flex-column gap-2'>
-      <div className='d-flex justify-content-between'>
-        <h1 className='text-light'>Manage my Sites</h1>
-
-        <button className='btn btn-secondary' type='button'>Create a New Site</button>
-      </div>
-      <div className="row">
-        {renderSiteCards()}
-      </div>
-      <div className='bg-white rounded-2 px-3 py-2' >
-        <h2>Average Annual Success</h2>
-        </div>
+  const renderSuccessRatesChart = () => (
+    <div>
+      <span>Chart to be rendered here</span>
     </div>
-  </div>
-)
-  }
+  )
+
+  return (
+    <div>
+      <div className='container pt-3 d-flex flex-column gap-2'>
+        <div className='d-flex justify-content-between'>
+          <h1 className='text-light'>Manage my Sites</h1>
+
+          <button className='btn btn-secondary' type='button'>Create a New Site</button>
+        </div>
+
+        <div className="mt-2 row gx-3 gy-3">
+          {renderSiteCards()}
+        </div>
+
+        <div className='mt-4 bg-white rounded p-3' >
+          <h2>Average Annual Success Rate Per Site</h2>
+          {renderSuccessRatesChart()}
+        </div>
+
+        <div className='mt-4 bg-white rounded p-3' >
+          <div className='d-flex justify-content-between'>
+            <h2>Batch Tracking</h2>
+            <div>
+              <span>Filters Go Here</span>
+            </div>
+          </div>
+          {renderSuccessRatesChart()}
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export default Analytics
