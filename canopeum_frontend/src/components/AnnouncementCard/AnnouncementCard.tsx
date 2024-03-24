fd8dfcfd386ef2a89b11625877f2f03608e2ab79
Audrey Baniant <audrey.baniant@gmail.com>
2024-03-24 12:01:44 -0400

    Added dynamic content

--- canopeum_frontend/src/components/AnnouncementCard/AnnouncementCard.tsx
+++ canopeum_frontend/src/components/AnnouncementCard/AnnouncementCard.tsx
@@ -1,20 +1,61 @@
 import './AnnouncementCard.scss';
-// import '../../services/api.ts'
-// import '../../../canopeum-mockoon.json'
 
-const AnnouncementCard = ({ siteId }) => (
-    <div className='card rounded px-3 py-2 col-3'>
+import { useEffect, useState } from 'react'
+
+import type { Site } from '../../services/api'
+import api from '../../services/apiInterface'
+import { ensureError } from '../../services/errors'
+
+const AnnouncementCard = ({ siteId }: { readonly siteId: number }) => {
+  const [site, setSite] = useState<Site>()
+  const [isLoadingSite, setIsLoadingSite] = useState(false)
+  const [SiteError, setSiteError] = useState<Error | undefined>(undefined)
+
+  const fetchSites = async () => {
+    setIsLoadingSite(true)
+    try {
+      const response = await api().social.site(siteId)
+      setSite(response)
+    } catch (error: unknown) {
+      setSiteError(ensureError(error))
+    } finally {
+      setIsLoadingSite(false)
+    }
+  }
+
+  useEffect((): void => {
+    void fetchSites()
+  }, [])
+
+  const renderAnnouncementCard = () => {
+    if (isLoadingSite) {
+      return <p>Loading...</p>
+    }
+
+    if (SiteError) {
+      return <p>Error: {SiteError.message}</p>
+    }
+
+    return (
       <div className='card-body'>
         <div className='d-flex justify-content-between align-items-center'>
           <h2 className='card-title'>Announcement</h2>
           <span className='material-symbols-outlined'>edit_square</span>
         </div>
-          <p className='card-text'>
-            Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
-          </p>
-          <a href='{siteId.link}'>Go somewhere</a>
+        <p className='card-text'>
+          {site?.announcement.body}
+        </p>
+        <a href={site?.announcement.link}>{site?.announcement.link}</a>
       </div>
+    )
+  }
+
+
+  return(
+    <div className='card rounded px-3 py-2 col-3'>
+      {renderAnnouncementCard()}
     </div>
   )
+}
 
 export default AnnouncementCard;
--- canopeum_frontend/src/pages/MapSite.tsx
+++ canopeum_frontend/src/pages/MapSite.tsx
@@ -3,13 +3,13 @@ import { useParams } from 'react-router-dom'
 import AnnouncementCard from '../components/AnnouncementCard/AnnouncementCard';
 
 const MapSite = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars -- We plan on using it
+   
   const { siteId } = useParams()
 
   return (
     <div>
       <h1>Map Site</h1>
-      <AnnouncementCard siteId={ siteId } />
+      <AnnouncementCard siteId={ Number(siteId) } />
     </div>
   )
 }
