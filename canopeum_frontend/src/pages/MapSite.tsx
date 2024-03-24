484449308e6f0c835e0cc70b1c18594223d7bece
Lucie Plantier <lucie.plantier@live.fr>
2024-03-24 12:00:43 -0400

    Adding siteId props to ContactCard

--- canopeum_frontend/src/pages/MapSite.tsx
+++ canopeum_frontend/src/pages/MapSite.tsx
@@ -3,15 +3,15 @@ import { useParams } from 'react-router-dom'
 import ContactCard  from '../components/ContactCard/ContactCard';
 
 const MapSite = () => {
-  /* eslint-disable @typescript-eslint/no-unused-vars -- We plan on using it */
+   
   // @ts-expect-error: We plan on using it
   const { siteId } = useParams()
-  /* eslint-enable @typescript-eslint/no-unused-vars */
+   
 
   return (
     <div>
       <h1>Map Site</h1>
-      <ContactCard />
+      <ContactCard siteId={siteId}/>
     </div>
   )
 }
