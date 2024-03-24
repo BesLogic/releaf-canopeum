23d1b41275e2db62e8412ac9f08fcaac087baa6a
Lucie Plantier <lucie.plantier@live.fr>
2024-03-24 15:21:01 -0400

    Fixing data transmission + Adding new social icons
    - No need for fetch anymore, been done on MapSite just passing the
    correct props

--- canopeum_frontend/src/components/ContactCard/ContactCard.tsx
+++ canopeum_frontend/src/components/ContactCard/ContactCard.tsx
@@ -1,10 +1,10 @@
 import '../ContactCard/contact-card.scss';
 import '../../pages/Utilities';
 
-import facebookLogo from '@assets/icons/facebook-regular.svg';
-import instagramLogo from '@assets/icons/instagram-regular.svg';
-import linkedinLogo from '@assets/icons/linkedin.svg';
-import twitterLogo from '@assets/icons/twitter-regular.svg';
+import facebookLogo from '@assets/icons/facebook-contact-logo.svg';
+import instagramLogo from '@assets/icons/instagram-contact-logo.svg';
+import linkedinLogo from '@assets/icons/linkedin-contact-logo.svg';
+import xLogo from '@assets/icons/x-contact-logo.svg';
 import { useEffect,useState } from 'react';
 
 import type { Contact } from '../../services/api'
@@ -13,73 +13,46 @@ import { ensureError } from '../../services/errors';
 
 
 
-const ContactCard = ({ siteId }:{readonly siteId: number}) => {
-  const [contact, setContact] = useState<Contact>();
-  const [isLoadingContactCard, setIsLoadingContactCard] = useState(false)
-  const [contactError, setContactError] = useState<Error | undefined>(undefined)
+const ContactCard = ({ contact }:{readonly contact: Contact}) => {
 
-  const fetchContact = async () => {
-    setIsLoadingContactCard(true)
-    try {
-    const response = (await api().social.site(siteId)).contact
-    setContact(response)
-    } catch (error: unknown) {
-      setContactError(ensureError(error))
-    } finally {
-      setIsLoadingContactCard(false)
-    }
-  }
-
-  useEffect((): void => {
-    void fetchContact()
-  }, [])
-
-  const renderContactCard = () => {
-    if (isLoadingContactCard) {
-      return <p>Loading contact ...</p>
-    }
-    if (contactError) {
-      return <p>Error: {contactError.message}</p>
-    }
-
-    return (
-      <div className='card col-4 rounded px-3 py-2'>
+  const renderContactCard = () => (
+      <div className='card my-2 rounded px-3 py-2'>
         <div className='card-body'>
             <div className='d-flex justify-content-between align-items-center pb-3'>
               <h2 className='card-title'>Contact</h2>
-              <span className='material-symbols-outlined'>edit_square</span>
+              <span className='material-symbols-outlined edit-btn'>edit_square</span>
             </div>
             <div className='info-section d-flex flex-column'>
               <div className='card-text adress d-flex align-items-center pb-3'>
                 <span className='material-symbols-outlined'>home_work</span>
-                <p className='mb-0'>{contact?.address}</p>
+                <p className='mb-0'>{contact.address}</p>
               </div>
               <div className='email d-flex align-items-center pb-3'>
                 <span className='material-symbols-outlined'>mail</span>
-                <p className='mb-0'>{contact?.email}</p>
+                <p className='mb-0'>{contact.email}</p>
               </div>
               <div className='phone d-flex align-items-center pb-3'>
                 <span className='material-symbols-outlined'>perm_phone_msg</span>
-                <p className='mb-0'>{contact?.phone}</p>
+                <p className='mb-0'>{contact.phone}</p>
               </div>
             </div>
             <div className='social-icons d-flex flex-row-reverse pt-3'>
-              <a href={contact?.linkedinLink}>
+              <a href={contact.linkedinLink}>
                 <img alt='linkedin-logo' className="px-2" src={linkedinLogo} />
               </a>
-              <a href={contact?.instagramLink}>
+              <a href={contact.instagramLink}>
                 <img alt="instagram-logo" className="px-2" src={instagramLogo} />
               </a>
-              <a href={contact?.xLink}>
-                <img alt="twitter-logo" className="px-2" src={twitterLogo} />
+              <a href={contact.xLink}>
+                <img alt="x-logo" className="px-2" src={xLogo} />
               </a>
-              <a href={contact?.facebookLink}>
+              <a href={contact.facebookLink}>
                 <img alt='facebook-logo' className="px-2" src={facebookLogo}/>
               </a>
             </div>
           </div>
       </div>
-  )}
+  )
 
   return (
   <div>
