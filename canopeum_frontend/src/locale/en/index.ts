05a003a45552dd78ca86b1933d9247af60931dc5
Nicolas Dontigny <nicolas.dontigny@beslogic.com>
2024-03-27 17:02:29 -0400

    Added basic site details layout + translations

--- canopeum_frontend/src/locale/en/index.ts
+++ canopeum_frontend/src/locale/en/index.ts
@@ -1,4 +1,5 @@
 import analyticsJSON from './analytics.json'
+import analyticsSiteJSON from './analyticsSite.json'
 import homeJSON from './home.json'
 import mapSiteJSON from './mapSite.json'
 
@@ -6,6 +7,7 @@ const enJSON = {
   translation: {
     home: { ...homeJSON },
     analytics: { ...analyticsJSON },
+    analyticsSite: { ...analyticsSiteJSON },
     mapSite: { ...mapSiteJSON },
   },
 }
