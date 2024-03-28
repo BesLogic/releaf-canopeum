8531c15d923b02674c0d7002426a90cf9e0c950a
Théo Auffeuvre <79534984+theo-auffeuvre@users.noreply.github.com>
2024-03-28 14:47:14 -0400

    Refactoring (#76)
    * WIP
    
    * merging all parts

--- canopeum_frontend/src/pages/MapSite.tsx
+++ canopeum_frontend/src/pages/MapSite.tsx
@@ -7,22 +7,8 @@ import { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import CreatePostWidget from '../components/CreatePostWidget'
 import PostWidget from '../components/PostWidget'
-import AnnouncementCard from '../components/AnnouncementCard/AnnouncementCard';
-
-
-import ContactCard  from '../components/ContactCard/ContactCard';
-import SiteSummaryCard from '../components/site/SiteSummaryCard'
-import type { SiteSocial } from '../services/api'
-import api from '../services/apiInterface'
-
-const fetchSite = async (siteId: number, setSite: (site: SiteSocial) => void) => {
-  try {
-    const site = await api().social.site(siteId)
-    setSite(site)
-  } catch (error) {
-    console.error(error)
-  }
-}
+import AnnouncementCard from '../components/AnnouncementCard'
+import ContactCard from '../components/ContactCard'
 
 const MapSite = () => {
   const { siteId } = useParams()
@@ -53,7 +39,7 @@ const MapSite = () => {
   }, [siteId])
 
   return (
-    <div className='container mt-2 d-flex flex-column gap-2'>
+    <div className='container mt-2 d-flex flex-column gap-4' style={{ padding: '1rem 10rem' }}>
       {isLoadingSite
         ? (
           <div className='bg-white rounded-2 2 py-2'>
@@ -70,17 +56,17 @@ const MapSite = () => {
       <div className='container px-0'>
         <div className='row'>
           <div className='col-4'>
-            <div className='d-flex flex-column gap-2'>
-              { site?.announcement && <AnnouncementCard announcement={site.announcement} />}
-              { site?.contact && <ContactCard contact={site.contact} />}
+            <div className='d-flex flex-column gap-4'>
+              {site?.announcement && <AnnouncementCard announcement={site.announcement} />}
+              {site?.contact && <ContactCard contact={site.contact} />}
             </div>
           </div>
           <div className='col-8'>
-            <div className='rounded-2 d-flex flex-column gap-2'>
+            <div className='rounded-2 d-flex flex-column gap-4'>
               {site && (
                 <>
                   <CreatePostWidget site={site} />
-                  <div className='d-flex flex-column gap-2'>
+                  <div className='d-flex flex-column gap-4'>
                     {site.posts?.map((post: Post) => <PostWidget key={post.id} post={post} />)}
                   </div>
                 </>
