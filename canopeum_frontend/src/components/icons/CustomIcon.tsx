1ae158d067aa4924f3b5deb17ba59a2520a50598
Nicolas Dontigny <45007853+NicolasDontigny@users.noreply.github.com>
2024-04-01 13:42:43 -0400

    Feature/33 specific site header (#77)
    * Started analytics page
    
    * Displaying responsive cards & all sections
    
    * Added site summary card translations
    
    * Added basic site details layout + translations
    
    * Added icon sizing classes
    
    * Added site summary admins, id, sponsors list
    
    * Updated site summary responses mockoon
    
    * Added custom icons
    
    * Added admins & address
    
    * Created Analytics site top card
    
    * Custom progressbar component, added type and remaining style
    
    * Removed unused useCallback
    
    ---------
    
    Co-authored-by: Nicolas Dontigny <nicolas.dontigny@beslogic.com>

---
+++ canopeum_frontend/src/components/icons/CustomIcon.tsx
@@ -0,0 +1,34 @@
+import { customIcons, type CustomIconType } from './customIconList'
+
+// These sizes match the custom icon scss sizes determined in App.scss
+export type IconSize =
+  // eslint-disable-next-line @typescript-eslint/sort-type-constituents -- Sort them in order of size
+  | '2xs'
+  | 'xs'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | '2xl'
+  | '3xl'
+  | '4xl'
+  | '5xl'
+  | '6xl'
+
+type Props = {
+  readonly icon: CustomIconType,
+  readonly size?: IconSize,
+}
+
+// TODO(NicolasDontigny): Find the best way to set the svg's fill color
+// This is currently not possible using the <img> tag in the CustomIcon component
+const CustomIcon = ({ icon, size }: Props) => {
+  let iconClassName = 'custom-icon'
+  if (size) {
+    iconClassName += ` icon-${size}`
+  }
+
+  return <img alt={icon} className={iconClassName} src={customIcons[icon]} />
+}
+
+export default CustomIcon
--- canopeum_frontend/src/pages/AnalyticsSite.tsx
+++ canopeum_frontend/src/pages/AnalyticsSite.tsx
@@ -1,16 +1,23 @@
-import type { Site } from '@services/api'
+import SiteSponsorProgress from '@components/analytics/SiteSponsorProgress'
+import { LanguageContext } from '@components/context/LanguageContext'
+import CustomIconBadge from '@components/CustomIconBadge'
+import type { SiteSummary } from '@services/api'
 import api from '@services/apiInterface'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+
+import CustomIcon from '../components/icons/CustomIcon'
+import classes from './AnalyticsSite.module.scss'
 
 const AnalyticsSite = () => {
-  const { t: translate } = useTranslation()
+  const { t: translate } = useTranslation<'analytics'>()
   const { siteId: siteIdFromParams } = useParams()
+  const { translateValue } = useContext(LanguageContext)
 
-  const [site, setSite] = useState<Site | undefined>()
+  const [site, setSite] = useState<SiteSummary | undefined>()
 
-  const fetchSite = async (siteId: number) => setSite(await api().analytics.site(siteId))
+  const fetchSite = async (siteId: number) => setSite(await api().analytics.siteSummary(siteId))
 
   useEffect(() => {
     if (!siteIdFromParams) return
@@ -21,6 +28,10 @@ const AnalyticsSite = () => {
     void fetchSite(siteIdNumber)
   }, [siteIdFromParams])
 
+  if (!site) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div className='container py-3 d-flex flex-column gap-4'>
       <div className='bg-white rounded d-flex'>
@@ -28,8 +39,74 @@ const AnalyticsSite = () => {
           <span>{translate('analyticsSite.location')}</span>
         </div>
 
-        <div className='site-info-container py-3 px-4'>
-          <h2>{site?.name}</h2>
+        <div className='site-info-container pb-4 pt-5 px-5 flex-grow-1'>
+          <div className='d-flex justify-content-between align-items-start'>
+            <div>
+              <h2>{site.name}</h2>
+              <div className='d-flex align-items-center'>
+                <CustomIconBadge icon='siteTypeCanopeumIcon' />
+                <span className='ms-2'>{translateValue(site.siteType)}</span>
+              </div>
+            </div>
+
+            <Link className='nav-link' to={`/map/${site.id}`}>
+              <button className='btn btn-primary' type='button'>{translate('analyticsSite.social-page')}</button>
+            </Link>
+          </div>
+
+          <div className={`d-flex justify-content-between mt-4 ${classes.analyticsCountsContainer}`}>
+            <div className='d-flex align-items-center gap-2 me-5'>
+              <div className='bg-lightgreen rounded-circle d-flex justify-content-center align-items-center p-2'>
+                <CustomIcon icon='sitePlantedIcon' size='5xl' />
+              </div>
+              <div className='d-flex flex-column'>
+                <span className='text-primary fs-4 fw-bold'>{site.plantCount}</span>
+                <span className='text-muted'>{translate('analytics.site-summary.planted')}</span>
+              </div>
+            </div>
+
+            <div className='d-flex align-items-center gap-2 me-5'>
+              <div className='bg-lightgreen rounded-circle d-flex justify-content-center align-items-center p-2'>
+                <CustomIcon icon='siteSurvivedIcon' size='5xl' />
+              </div>
+              <div className='d-flex flex-column'>
+                <span className='text-primary fs-4 fw-bold'>{site.survivedCount}</span>
+                <span className='text-muted'>{translate('analytics.site-summary.survived')}</span>
+              </div>
+            </div>
+
+            <div className='d-flex align-items-center gap-2 me-5'>
+              <div className='bg-lightgreen rounded-circle d-flex justify-content-center align-items-center p-2'>
+                <CustomIcon icon='sitePropagationIcon' size='5xl' />
+              </div>
+              <div className='d-flex flex-column'>
+                <span className='text-primary fs-4 fw-bold'>{site.propagationCount}</span>
+                <span className='text-muted'>{translate('analytics.site-summary.propagation')}</span>
+              </div>
+            </div>
+
+            <div className='d-flex align-items-center gap-2 me-5'>
+              <div className='bg-lightgreen rounded-circle d-flex justify-content-center align-items-center p-2'>
+                <CustomIcon icon='siteVisitorsIcon' size='5xl' />
+              </div>
+              <div className='d-flex flex-column'>
+                <span className='text-primary fs-4 fw-bold'>{site.visitorCount}</span>
+                <span className='text-muted'>{translate('analytics.site-summary.visitors')}</span>
+              </div>
+            </div>
+          </div>
+
+          <div className='mt-4'>
+            <div className='d-flex align-items-flex-end fw-bold'>
+              <span className='material-symbols-outlined'>group</span>
+              <span className='ms-1 me-2'>{translate('analyticsSite.sponsors')}:</span>
+              {site.sponsors.map(sponsor => <span className='me-4' key={sponsor}>{sponsor}</span>)}
+            </div>
+          </div>
+
+          <div className='mt-1'>
+            <SiteSponsorProgress progress={site.progress} />
+          </div>
         </div>
       </div>
 
