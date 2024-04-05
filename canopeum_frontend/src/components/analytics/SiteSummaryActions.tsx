00e7ec6b77c909ab9513c468c1e704ab4308bfe7
Nicolas Dontigny <45007853+NicolasDontigny@users.noreply.github.com>
2024-04-05 18:04:44 -0400

    Feature/31 site actions from summary card (#83)
    * Started analytics page
    
    * Displaying responsive cards & all sections
    
    * Fixed duplicated keys and extracted successRatesChart in component
    
    * analyticsSideHeader component, added batch tracking & model date
    
    * last updated date display
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fixed api client change & linting issues
    
    * Added site summary card popover
    
    * Added rsuite popover + whisper
    
    * Created search bar component
    
    * Fixed model property and committed site images
    
    * Updated gitignore
    
    * Added search bar
    
    * Updated models and relations
    
    * Small fixes
    
    * Added admins in initial seeding + hiding password
    
    * Listing admins, checkboxes, search function
    
    * Added checkbox components & using for selecting admins
    
    * Added update site admins endpoint
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fixed merge conflicts and using new user model
    
    * Self pr review
    
    ---------
    
    Co-authored-by: Nicolas Dontigny <nicolas.dontigny@beslogic.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ canopeum_frontend/src/components/analytics/SiteSummaryActions.tsx
@@ -0,0 +1,123 @@
+import Checkbox from '@components/Checkbox'
+import SearchBar from '@components/SearchBar'
+import type { SiteSummary, User } from '@services/api'
+import getApiClient from '@services/apiInterface'
+import { useState } from 'react'
+import { Dropdown, Popover, Whisper } from 'rsuite'
+
+type Props = {
+  readonly siteSummary: SiteSummary,
+  readonly admins: User[],
+}
+
+const SiteSummaryActions = ({ siteSummary, admins }: Props) => {
+  const [filteredAdmins, setFilteredAdmins] = useState(admins)
+  const [selectedAdmins, setSelectedAdmins] = useState(siteSummary.admins.map(admin => admin.user))
+
+  const onSearchAdmins = (query: string) =>
+    setFilteredAdmins(admins.filter(admin =>
+      admin
+        .username
+        .toLocaleLowerCase()
+        .includes(query.toLocaleLowerCase())
+    ))
+
+  const onAdminSelectionChange = (adminId: number, isSelected: boolean) => {
+    if (isSelected) {
+      const isAlreadyAdmin = selectedAdmins.find(admin => admin.id === adminId)
+      if (isAlreadyAdmin) return
+
+      const matchingAdmin = admins.find(admin => admin.id === adminId)
+      if (!matchingAdmin) return
+
+      setSelectedAdmins(previous => [...previous, matchingAdmin])
+    } else {
+      setSelectedAdmins(previous => previous.filter(admin => admin.id !== adminId))
+    }
+  }
+
+  const onSaveAdmins = async () =>
+    await getApiClient()
+      .siteAdminsClient
+      .update(siteSummary.id, selectedAdmins.map(admin => admin.id))
+  // TODO(NicolasDontigny): Do we need to update the parent model here?
+
+  const onSelectAdminsCancel = () => {
+    setFilteredAdmins([...admins])
+    setSelectedAdmins(siteSummary.admins.map(admin => admin.user))
+  }
+
+  const administratorsSelection = (
+    <div className='py-2 px-3' style={{ minWidth: '18rem' }}>
+      <SearchBar onChange={onSearchAdmins} />
+
+      <div className='mt-2 py-2'>
+        {filteredAdmins.map(admin => {
+          const checkboxId = `site-${siteSummary.id}-admin-${admin.id}-checkbox`
+          const isSelected = selectedAdmins.some(siteAdmin => siteAdmin.id === admin.id)
+
+          return (
+            <Checkbox
+              checked={isSelected}
+              id={checkboxId}
+              key={admin.id}
+              onChange={onAdminSelectionChange}
+              value={admin.id}
+            >
+              {admin.username}
+            </Checkbox>
+          )
+        })}
+      </div>
+
+      <div className='d-flex justify-content-between pt-2 border-top'>
+        <button
+          className='btn btn-outline-primary'
+          onClick={onSelectAdminsCancel}
+          style={{ minWidth: '6rem' }}
+          type='button'
+        >Cancel</button>
+        <button
+          className='btn btn-primary'
+          onClick={onSaveAdmins}
+          style={{ minWidth: '6rem' }}
+          type='button'
+        >Save</button>
+      </div>
+    </div>
+  )
+
+  const actionsPopover = (
+    <Popover full>
+      <Dropdown.Menu>
+        <Dropdown.Menu title='Select Administrator'>
+          {administratorsSelection}
+        </Dropdown.Menu>
+        <Dropdown.Item>Edit Site Information</Dropdown.Item>
+        <Dropdown.Item>Delete</Dropdown.Item>
+      </Dropdown.Menu>
+    </Popover>
+  )
+
+  return (
+    <Whisper
+      placement='auto'
+      speaker={actionsPopover}
+      trigger='click'
+    >
+      <button
+        className='bg-lightgreen text-center rounded-circle unstyled-button'
+        type='button'
+      >
+        <span
+          className='material-symbols-outlined text-primary align-middle'
+          style={{ fontSize: 24 }}
+        >
+          more_horiz
+        </span>
+      </button>
+    </Whisper>
+  )
+}
+
+export default SiteSummaryActions
--- canopeum_frontend/src/components/analytics/SiteSummaryCard.tsx
+++ canopeum_frontend/src/components/analytics/SiteSummaryCard.tsx
@@ -1,5 +1,7 @@
 import SiteSponsorProgress from '@components/analytics/SiteSponsorProgress'
-import type { SiteSummary } from '@services/api'
+import SiteSummaryActions from '@components/analytics/SiteSummaryActions'
+import PrimaryIconBadge from '@components/PrimaryIconBadge'
+import type { SiteSummary, User } from '@services/api'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 
@@ -7,13 +9,14 @@ import CustomIcon from '../icons/CustomIcon'
 
 type Props = {
   readonly site: SiteSummary,
+  readonly admins: User[],
 }
 
-const SiteSummaryCard = ({ site }: Props) => {
+const SiteSummaryCard = ({ site, admins }: Props) => {
   const { t: translate } = useTranslation()
 
   const siteAdminsDisplay = site.admins.length > 0
-    ? site.admins.map(admin => admin.username).join(', ')
+    ? site.admins.map(admin => admin.user.username).join(', ')
     : translate('analytics.site-summary.no-admins')
 
   return (
@@ -21,63 +24,69 @@ const SiteSummaryCard = ({ site }: Props) => {
       className='col-3'
       key={site.name}
     >
-      <Link className='nav-link' to={`/analytics/${site.id}`}>
-        <div className='card h-100 w-100 py-3'>
-          <div className='card-body d-flex flex-column h-100'>
-            <div className='d-flex align-items-center card-title'>
-              <div className='bg-primary rounded-circle d-flex justify-content-center align-items-center p-1 me-2'>
-                <span className='material-symbols-outlined text-light'>school</span>
+      <div className='card h-100 w-100 py-3'>
+        <div className='card-body d-flex flex-column h-100'>
+          <div className='d-flex justify-content-between align-items-center card-title'>
+            <Link className='nav-link' to={`/analytics/${site.id}`}>
+              <div className='d-flex gap-1 align-items-center flex-grow-1'>
+                <PrimaryIconBadge type='school' />
+                <h5 className='mb-0 text-ellipsis'>{site.name ?? translate('analytics.site-summary.unnamed-site')}</h5>
               </div>
-              <h5 className='mb-0 text-ellipsis'>{site.name ?? translate('analytics.site-summary.unnamed-site')}</h5>
+            </Link>
+
+            {
+              /* TODO(NicolasDontigny): Once authentication & user permissions have been implemented,
+              require the mega_admin permission here */
+            }
+            <SiteSummaryActions admins={admins} siteSummary={site} />
+          </div>
+
+          <div className='card-subtitle my-1'>
+            <div className='d-flex align-items-center text-muted'>
+              <span className='material-symbols-outlined fill-icon text-muted me-1'>location_on</span>
+              <span className='text-ellipsis'>
+                {site.coordinate.address ?? translate('analytics.site-summary.unknown')}
+              </span>
+            </div>
+            <div className='d-flex align-items-center text-muted'>
+              <span className='material-symbols-outlined fill-icon text-muted me-1'>person</span>
+              <span className='text-ellipsis'>{siteAdminsDisplay}</span>
+            </div>
+          </div>
+
+          <div className='card-text mt-2'>
+            <div className='row my-2'>
+              <div className='col-4 d-flex flex-column align-items-center'>
+                <div className='bg-lightgreen rounded-circle d-flex justify-content-center align-items-center p-2'>
+                  <CustomIcon icon='sitePlantedIcon' size='xl' />
+                </div>
+                <span className='text-primary fs-4 fw-bold'>{site.plantCount}</span>
+                <span className='text-muted'>{translate('analytics.site-summary.planted')}</span>
+              </div>
+
+              <div className='col-4 d-flex flex-column align-items-center'>
+                <div className='bg-lightgreen rounded-circle d-flex justify-content-center align-items-center p-2'>
+                  <CustomIcon icon='siteSurvivedIcon' size='xl' />
+                </div>
+                <span className='text-primary fs-4 fw-bold'>{site.survivedCount}</span>
+                <span className='text-muted'>{translate('analytics.site-summary.survived')}</span>
+              </div>
+
+              <div className='col-4 d-flex flex-column align-items-center'>
+                <div className='bg-lightgreen rounded-circle d-flex justify-content-center align-items-center p-2'>
+                  <CustomIcon icon='sitePropagationIcon' size='xl' />
+                </div>
+                <span className='text-primary fs-4 fw-bold'>{site.propagationCount}</span>
+                <span className='text-muted'>{translate('analytics.site-summary.propagation')}</span>
+              </div>
             </div>
 
-            <div className='card-subtitle my-1'>
-              <div className='d-flex align-items-center text-muted'>
-                <span className='material-symbols-outlined fill-icon text-muted me-1'>location_on</span>
-                <span className='text-ellipsis'>
-                  {site.coordinate.address ?? translate('analytics.site-summary.unknown')}
-                </span>
-              </div>
-              <div className='d-flex align-items-center text-muted'>
-                <span className='material-symbols-outlined fill-icon text-muted me-1'>person</span>
-                <span className='text-ellipsis'>{siteAdminsDisplay}</span>
-              </div>
-            </div>
-
-            <div className='card-text mt-2'>
-              <div className='row my-2'>
-                <div className='col-4 d-flex flex-column align-items-center'>
-                  <div className='bg-lightgreen rounded-circle d-flex justify-content-center align-items-center p-2'>
-                    <CustomIcon icon='sitePlantedIcon' size='xl' />
-                  </div>
-                  <span className='text-primary fs-4 fw-bold'>{site.plantCount}</span>
-                  <span className='text-muted'>{translate('analytics.site-summary.planted')}</span>
-                </div>
-
-                <div className='col-4 d-flex flex-column align-items-center'>
-                  <div className='bg-lightgreen rounded-circle d-flex justify-content-center align-items-center p-2'>
-                    <CustomIcon icon='siteSurvivedIcon' size='xl' />
-                  </div>
-                  <span className='text-primary fs-4 fw-bold'>{site.survivedCount}</span>
-                  <span className='text-muted'>{translate('analytics.site-summary.survived')}</span>
-                </div>
-
-                <div className='col-4 d-flex flex-column align-items-center'>
-                  <div className='bg-lightgreen rounded-circle d-flex justify-content-center align-items-center p-2'>
-                    <CustomIcon icon='sitePropagationIcon' size='xl' />
-                  </div>
-                  <span className='text-primary fs-4 fw-bold'>{site.propagationCount}</span>
-                  <span className='text-muted'>{translate('analytics.site-summary.propagation')}</span>
-                </div>
-              </div>
-
-              <div className='mt-4'>
-                <SiteSponsorProgress progress={site.progress} />
-              </div>
+            <div className='mt-4'>
+              <SiteSponsorProgress progress={site.progress} />
             </div>
           </div>
         </div>
-      </Link>
+      </div>
     </div>
   )
 }
