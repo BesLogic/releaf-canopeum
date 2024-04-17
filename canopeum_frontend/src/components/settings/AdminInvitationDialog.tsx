d3935b3c4cfc4a42f3bfa2f6bffd89764561cc98
Nicolas Dontigny <nicolas.dontigny@beslogic.com>
2024-04-17 16:44:02 -0400

    Created admin invitation dialog

---
+++ canopeum_frontend/src/components/settings/AdminInvitationDialog.tsx
@@ -0,0 +1,21 @@
+import { Dialog, DialogContent } from '@mui/material'
+import { useTranslation } from 'react-i18next'
+
+type Props = {
+  readonly open: boolean,
+  readonly handleClose: () => void,
+}
+
+const AdminInvitationDialog = ({ open, handleClose }: Props) => {
+  const { t: translate } = useTranslation()
+
+  return (
+    <Dialog fullWidth maxWidth='sm' onClose={handleClose} open={open}>
+      <DialogContent className='pb-5'>
+        <div><span>HELLO</span></div>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default AdminInvitationDialog
