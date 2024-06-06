251f99b15fcbe26de8d8760732dfb5813bcb38a7
Theo Auffeuvre <theo.auffeuvre@beslogic.com>
2024-06-06 10:06:30 -0400

    WIP

--- canopeum_backend/canopeum_backend/settings.py
+++ canopeum_backend/canopeum_backend/settings.py
@@ -55,7 +55,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
