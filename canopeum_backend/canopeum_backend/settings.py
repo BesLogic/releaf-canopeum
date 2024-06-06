09f1afb782da0d71b2a822e2ae254e62b05e79fa
Theo Auffeuvre <theo.auffeuvre@beslogic.com>
2024-06-06 09:47:25 -0400

    WIP

--- canopeum_backend/canopeum_backend/settings.py
+++ canopeum_backend/canopeum_backend/settings.py
@@ -61,15 +61,18 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "http://api.canopeum.releaftrees.life",
-    "http://releaftrees.life",
-    "https://releaftrees.life",
-]
+
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://localhost:3000",
+#     "http://api.canopeum.releaftrees.life",
+#     "http://releaftrees.life",
+#     "https://releaftrees.life",
+# ]
 
 ROOT_URLCONF = "canopeum_backend.urls"
 
