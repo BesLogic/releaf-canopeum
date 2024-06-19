57e00f94ef00eecd02d0363b7fa1ef200fe574f7
Theo Auffeuvre <theo.auffeuvre@beslogic.com>
2024-06-19 16:51:08 -0400

    With entrypoint

---
+++ canopeum_backend/canopeum_backend/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+echo "Migrate the Database at startup of project"
+
+# Wait for few minute and run db migraiton
+while ! python manage.py migrate  2>&1; do
+   echo "Migration is in progress status"
+   sleep 3
+done
+
+echo "Migration is done"
+
+# Start the server
+exec "$@"
