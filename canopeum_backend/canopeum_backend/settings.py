a64c9b8ab0467b723cc4f0f4f153633aa4a80628
Theo Auffeuvre <theo.auffeuvre@beslogic.com>
2024-06-21 14:37:48 -0400

    Backend secret backend setted up v2

--- canopeum_backend/canopeum_backend/settings.py
+++ canopeum_backend/canopeum_backend/settings.py
@@ -224,4 +224,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 APPEND_SLASH = False
 
-GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+GOOGLE_API_KEY = get_secret("GOOGLE_API_KEY", "")
