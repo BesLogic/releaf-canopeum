7b0fb428554fcb0e02967cb191ae1e7b0dcaf46e
Samuel Therrien <samuel.therrien@beslogic.com>
2024-07-11 18:51:25 -0400

    non-empty secret mypy

--- canopeum_backend/canopeum_backend/settings.py
+++ canopeum_backend/canopeum_backend/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
-from typing import TYPE_CHECKING
 
 import dj_database_url
 from corsheaders.defaults import default_headers
@@ -40,7 +39,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret("SECRET_KEY_DJANGO_CANOPEUM", "mypy-ext" if TYPE_CHECKING else "")
+SECRET_KEY = get_secret("SECRET_KEY_DJANGO_CANOPEUM", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = get_secret("DEBUG", "True")
