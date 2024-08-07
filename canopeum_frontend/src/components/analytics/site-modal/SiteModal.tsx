33dd866d9bc6fd6e5aa69441a11204cbde686d4a
Samuel Therrien <samuel.therrien@beslogic.com>
2024-08-07 18:21:28 -0400

    Lint file too big

--- canopeum_frontend/src/components/analytics/site-modal/SiteModal.tsx
+++ canopeum_frontend/src/components/analytics/site-modal/SiteModal.tsx
@@ -332,4 +332,5 @@ const SiteModal = ({ open, handleClose, siteId }: Props) => {
     </Dialog>
   )
 }
+// eslint-disable-next-line max-lines -- Will resolve itself with https://github.com/BesLogic/releaf-canopeum/pull/193
 export default SiteModal
