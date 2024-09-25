0225f0b6724fdff18a6334f3ebffec478b5cb660
Nicolas Dontigny <nicolas.dontigny@beslogic.com>
2024-09-25 17:51:07 -0400

    Lint fix

--- canopeum_frontend/src/components/analytics/AnalyticsSiteHeader.tsx
+++ canopeum_frontend/src/components/analytics/AnalyticsSiteHeader.tsx
@@ -2,13 +2,11 @@ import './AnalyticsSiteHeader.scss'
 
 import { useContext } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Link } from 'react-router-dom'
 
 import SiteCountBadge from '@components/analytics/SiteCountBadge'
 import SiteSponsorProgress from '@components/analytics/SiteSponsorProgress'
 import { LanguageContext } from '@components/context/LanguageContext'
 import CustomIconBadge from '@components/CustomIconBadge'
-import { appRoutes } from '@constants/routes.constant'
 import { getImageNameByWMOCategories } from '@constants/weatherImageMap'
 import type { SiteDetailSummary } from '@services/api'
 
