a124673a4a528e710212f389610ae9d289bdb4e9
Samuel T. <samuel.therrien@beslogic.com>
2024-10-01 16:05:14 -0400

    Update canopeum_backend/canopeum_backend/models.py

--- canopeum_backend/canopeum_backend/models.py
+++ canopeum_backend/canopeum_backend/models.py
@@ -166,7 +166,7 @@ class Site(models.Model):
             return 0
 
         batches = Batch.objects.filter(site=self)
-        sponsored_plant_count = sum(batch.plant_count() for specie in batches)
+        sponsored_plant_count = sum(batch.plant_count() for batch in batches)
         if sponsored_plant_count >= total_plant_count:
             return 100
 
