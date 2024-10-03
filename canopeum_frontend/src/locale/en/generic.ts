8a7e24ed42472496e19427339f73f18958b0307a
Samuel T. <samuel.therrien@beslogic.com>
2024-10-03 14:06:12 -0400

    Validate consistent shape of translation files (#259)

---
+++ canopeum_frontend/src/locale/en/generic.ts
@@ -0,0 +1,11 @@
+export default {
+  cancel: 'Cancel',
+  delete: 'Delete',
+  ok: 'Ok',
+  submit: 'Submit',
+  subscribe: 'Subscribe',
+  'copied-clipboard': 'Copied to clipboard',
+  'error-default': 'Oooops! There was a problem while loading the data.',
+  upload: 'Upload',
+  edit: 'Edit',
+}
