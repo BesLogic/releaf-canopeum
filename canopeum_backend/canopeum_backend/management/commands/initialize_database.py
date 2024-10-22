7c3ada0675f2d0e73ed94cfa7e2d9c3e3bc26947
Samuel Therrien <samuel.therrien@beslogic.com>
2024-10-22 15:29:58 -0400

    Update some init code

--- canopeum_backend/canopeum_backend/management/commands/initialize_database.py
+++ canopeum_backend/canopeum_backend/management/commands/initialize_database.py
@@ -39,105 +39,105 @@ seeding_images_path = (
     Path(canopeum_backend.settings.BASE_DIR) / "canopeum_backend" / "seeding" / "images"
 )
 
-tree_types = [
-    ["Balsam Fir", "Sapin baumier"],
-    ["Black Maple", "Érable noir"],
-    ["Red Maple", "Érable rouge"],
-    ["Silver Maple", "Érable argenté"],
-    ["Sugar Maple", "Érable à sucre"],
-    ["Ohio Buckeye", "Marronnier d'Ohio"],
-    ["Speckled Alder", "Aulne tacheté"],
-    ["Green Alder", "Aulne vert"],
-    ["Serviceberries", "Amélanchier"],
-    ["Black Chokeberry", "Aronie noire"],
-    ["Yellow Birch", "Bouleau jaune"],
-    ["White Birch", "Bouleau blanc"],
-    ["Gray Birch", "Bouleau gris"],
-    ["Blue Beech", "Hêtre bleu"],
-    ["Bitternut Hickory", "Noyer amer"],
-    ["Pignut Hickory", "Noyer des montagnes"],
-    ["Big Shellbark Hickory", "Noyer à gros fruits"],
-    ["Shagbark Hickory", "Noyer à écorce shaggy"],
-    ["American Bittersweet", "Morelle d'Amérique"],
-    ["Northern Hackberry", "Micocoulier occidental"],
-    ["Buttonbush", "Céphalanthère occidentale"],
-    ["Redbud", "Gainier de Virginie"],
-    ["Alternate-leaf Dogwood", "Cornouiller à feuilles alternes"],
-    ["Silky Dogwood", "Cornouiller soyeux"],
-    ["Flowering Dogwood", "Cornouiller à fleurs"],
-    ["Gray Dogwood", "Cornouiller gris"],
-    ["Redosier Dogwood", "Cornouiller stolonifère"],
-    ["American Hazelnut", "Noisetier d'Amérique"],
-    ["Beaked Hazel", "Coudrier à bec"],
-    ["Hawthorns", "Aubépines"],
-    ["Bush Honeysuckle", "Chèvrefeuille arbustif"],
-    ["American Beech", "Hêtre d'Amérique"],
-    ["White Ash", "Frêne blanc"],
-    ["Black Ash", "Frêne noir"],
-    ["Green/ Red Ash", "Frêne vert/rouge"],
-    ["Pumpkin Ash", "Frêne citrouille"],
-    ["Blue Ash", "Frêne bleu"],
-    ["Honey Locust", "Faux-acacia"],
-    ["Kentucky Coffeetree", "Gymnocladus de Kentucky"],
-    ["Witch-Hazel", "Hamamélis de Virginie"],
-    ["Winterberry", "Houx d'hiver"],
-    ["Butternut", "Noyer cendré"],
-    ["Black Walnut", "Noyer noir"],
-    ["Common Juniper", "Genévrier commun"],
-    ["Eastern Red Cedar", "Genévrier rouge de l'Est"],
-    ["Tamarac", "Mélèze laricin"],
-    ["Spicebush", "Lindera à feuilles opposées"],
-    ["Tulip-Tree", "Tulipier de Virginie"],
-    ["Cucumber-Tree", "Magnolia concombre"],
-    ["Sweetgale", "Myrica gale"],
-    ["Black Gum", "Nyssa sylvatica"],
-    ["Ironwood", "Ostryer de Virginie"],
-    ["Virginia Creeper", "Vigne vierge"],
-    ["Ninebark", "Physocarpe"],
-    ["White Spruce", "Épinette blanche"],
-    ["Black Spruce", "Épinette noire"],
-    ["Red Spruce", "Épinette rouge"],
-    ["Jack Pine", "Pin gris"],
-    ["Red Pine", "Pin rouge"],
-    ["Pitch Pine", "Pin rigide"],
-    ["Eastern White Pine", "Pin blanc de l'Est"],
-    ["Sycamore", "Platanus occidentalis"],
-    ["Eastern Cottonwood", "Peuplier baumier"],
-    ["Trembling Aspen", "Peuplier faux-tremble"],
-    ["American Plum", "Prunier d'Amérique"],
-    ["Pin Cherry", "Cerisier de Pennsylvanie"],
-    ["Black Cherry", "Cerisier noir"],
-    ["Eastern Choke Cherry", "Cerisier de Virginie"],
-    ["Hoptree", "Ptelea trifoliata"],
-    ["White Oak", "Chêne blanc"],
-    ["Swamp White Oak", "Chêne blanc des marais"],
-    ["Bur Oak", "Chêne bur"],
-    ["Chinquapin Oak", "Chêne chinkapin"],
-    ["Pin Oak", "Chêne à pin"],
-    ["Red Oak", "Chêne rouge"],
-    ["Shumard Oak", "Chêne de Shumard"],
-    ["Black Oak", "Chêne noir"],
-    ["Fragrant Sumac", "Sumac aromatique"],
-    ["Shining Sumac", "Sumac brillant"],
-    ["Staghorn Sumac", "Sumac vinaigrier"],
-    ["Purple-flowering Raspberry", "Ronce pourpre"],
-    ["Roses", "Roses"],
-    ["Willows", "Saules"],
-    ["Black Elderberry", "Sureau noir"],
-    ["Red Elderberry", "Sureau rouge"],
-    ["Sassafras", "Sassafras"],
-    ["American Mountain Ash", "Sorbier d'Amérique"],
-    ["Showy Mountain Ash", "Sorbier remarquable"],
-    ["Narrow-leaved Meadowsweet", "Reine-des-prés à feuilles étroites"],
-    ["Snowberry", "Symphorine"],
-    ["White Cedar", "Thuya occidental"],
-    ["Basswood", "Tilleul d'Amérique"],
-    ["Eastern Hemlock", "Pruche de l'Est"],
-    ["American Elm", "Orme d'Amérique"],
-    ["Slippery Elm", "Orme glissant"],
-    ["Nannyberry", "Viorne à feuilles de viorne"],
-    ["American Highbush Cranberry", "Viorne trilobée"],
-]
+tree_type_names = (
+    ("Balsam Fir", "Sapin baumier"),
+    ("Black Maple", "Érable noir"),
+    ("Red Maple", "Érable rouge"),
+    ("Silver Maple", "Érable argenté"),
+    ("Sugar Maple", "Érable à sucre"),
+    ("Ohio Buckeye", "Marronnier d'Ohio"),
+    ("Speckled Alder", "Aulne tacheté"),
+    ("Green Alder", "Aulne vert"),
+    ("Serviceberries", "Amélanchier"),
+    ("Black Chokeberry", "Aronie noire"),
+    ("Yellow Birch", "Bouleau jaune"),
+    ("White Birch", "Bouleau blanc"),
+    ("Gray Birch", "Bouleau gris"),
+    ("Blue Beech", "Hêtre bleu"),
+    ("Bitternut Hickory", "Noyer amer"),
+    ("Pignut Hickory", "Noyer des montagnes"),
+    ("Big Shellbark Hickory", "Noyer à gros fruits"),
+    ("Shagbark Hickory", "Noyer à écorce shaggy"),
+    ("American Bittersweet", "Morelle d'Amérique"),
+    ("Northern Hackberry", "Micocoulier occidental"),
+    ("Buttonbush", "Céphalanthère occidentale"),
+    ("Redbud", "Gainier de Virginie"),
+    ("Alternate-leaf Dogwood", "Cornouiller à feuilles alternes"),
+    ("Silky Dogwood", "Cornouiller soyeux"),
+    ("Flowering Dogwood", "Cornouiller à fleurs"),
+    ("Gray Dogwood", "Cornouiller gris"),
+    ("Redosier Dogwood", "Cornouiller stolonifère"),
+    ("American Hazelnut", "Noisetier d'Amérique"),
+    ("Beaked Hazel", "Coudrier à bec"),
+    ("Hawthorns", "Aubépines"),
+    ("Bush Honeysuckle", "Chèvrefeuille arbustif"),
+    ("American Beech", "Hêtre d'Amérique"),
+    ("White Ash", "Frêne blanc"),
+    ("Black Ash", "Frêne noir"),
+    ("Green/ Red Ash", "Frêne vert/rouge"),
+    ("Pumpkin Ash", "Frêne citrouille"),
+    ("Blue Ash", "Frêne bleu"),
+    ("Honey Locust", "Faux-acacia"),
+    ("Kentucky Coffeetree", "Gymnocladus de Kentucky"),
+    ("Witch-Hazel", "Hamamélis de Virginie"),
+    ("Winterberry", "Houx d'hiver"),
+    ("Butternut", "Noyer cendré"),
+    ("Black Walnut", "Noyer noir"),
+    ("Common Juniper", "Genévrier commun"),
+    ("Eastern Red Cedar", "Genévrier rouge de l'Est"),
+    ("Tamarac", "Mélèze laricin"),
+    ("Spicebush", "Lindera à feuilles opposées"),
+    ("Tulip-Tree", "Tulipier de Virginie"),
+    ("Cucumber-Tree", "Magnolia concombre"),
+    ("Sweetgale", "Myrica gale"),
+    ("Black Gum", "Nyssa sylvatica"),
+    ("Ironwood", "Ostryer de Virginie"),
+    ("Virginia Creeper", "Vigne vierge"),
+    ("Ninebark", "Physocarpe"),
+    ("White Spruce", "Épinette blanche"),
+    ("Black Spruce", "Épinette noire"),
+    ("Red Spruce", "Épinette rouge"),
+    ("Jack Pine", "Pin gris"),
+    ("Red Pine", "Pin rouge"),
+    ("Pitch Pine", "Pin rigide"),
+    ("Eastern White Pine", "Pin blanc de l'Est"),
+    ("Sycamore", "Platanus occidentalis"),
+    ("Eastern Cottonwood", "Peuplier baumier"),
+    ("Trembling Aspen", "Peuplier faux-tremble"),
+    ("American Plum", "Prunier d'Amérique"),
+    ("Pin Cherry", "Cerisier de Pennsylvanie"),
+    ("Black Cherry", "Cerisier noir"),
+    ("Eastern Choke Cherry", "Cerisier de Virginie"),
+    ("Hoptree", "Ptelea trifoliata"),
+    ("White Oak", "Chêne blanc"),
+    ("Swamp White Oak", "Chêne blanc des marais"),
+    ("Bur Oak", "Chêne bur"),
+    ("Chinquapin Oak", "Chêne chinkapin"),
+    ("Pin Oak", "Chêne à pin"),
+    ("Red Oak", "Chêne rouge"),
+    ("Shumard Oak", "Chêne de Shumard"),
+    ("Black Oak", "Chêne noir"),
+    ("Fragrant Sumac", "Sumac aromatique"),
+    ("Shining Sumac", "Sumac brillant"),
+    ("Staghorn Sumac", "Sumac vinaigrier"),
+    ("Purple-flowering Raspberry", "Ronce pourpre"),
+    ("Roses", "Roses"),
+    ("Willows", "Saules"),
+    ("Black Elderberry", "Sureau noir"),
+    ("Red Elderberry", "Sureau rouge"),
+    ("Sassafras", "Sassafras"),
+    ("American Mountain Ash", "Sorbier d'Amérique"),
+    ("Showy Mountain Ash", "Sorbier remarquable"),
+    ("Narrow-leaved Meadowsweet", "Reine-des-prés à feuilles étroites"),
+    ("Snowberry", "Symphorine"),
+    ("White Cedar", "Thuya occidental"),
+    ("Basswood", "Tilleul d'Amérique"),
+    ("Eastern Hemlock", "Pruche de l'Est"),
+    ("American Elm", "Orme d'Amérique"),
+    ("Slippery Elm", "Orme glissant"),
+    ("Nannyberry", "Viorne à feuilles de viorne"),
+    ("American Highbush Cranberry", "Viorne trilobée"),
+)
 
 
 def create_posts_for_site(site):
@@ -165,7 +165,7 @@ def create_posts_for_site(site):
 
 def create_batch_species_for_batch(batch):
     num_species = random.randint(4, 8)
-    total_tree_types = len(tree_types)
+    total_tree_types = len(tree_type_names)
 
     # Ensure num_species is not greater than the total number
     # of tree types available to avoid an infinite loop
@@ -185,7 +185,7 @@ def create_batch_species_for_batch(batch):
         )
 
 
-batch_names = [
+batch_names = (
     "First Batch",
     "Second Batch",
     "Third Batch",
@@ -194,7 +194,7 @@ batch_names = [
     "Sixth Batch",
     "Seventh Batch",
     "Eight Batch",
-]
+)
 
 sponsor_names = [
     "Green Earth Initiative",
@@ -377,60 +377,64 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS("Data Generated"))
 
     def create_fertilizer_types(self):
-        fertilizer_types = [
-            ["Synthetic", "Synthétique"],
-            ["Inoculant", "Inoculant"],
-            ["Organic compost", "Compost organique"],
-            ["Manure", "Fumier"],
-            ["Bone meal", "Farine d'os"],
-            ["Fish emulsion", "Émulsion de poisson"],
-            ["Blood meal", "Farine de sang"],
-            ["Seaweed fertilizer", "Engrais d'algues"],
-            ["Bat guano", "Guano de chauve-souris"],
-            ["Worm castings", "Moulée de vers"],
-            ["Compost tea", "Thé de compost"],
-            ["Wood ash", "Cendre de bois"],
-            ["Rock phosphate", "Phosphate de roche"],
-            ["Greensand", "Sable vert"],
-            ["Alfalfa meal", "Farine d'alfalfa"],
-            ["Cottonseed meal", "Farine de tourteau de coton"],
-            ["Feather meal", "Farine de plumes"],
-            ["Humic acid", "Acide humique"],
-        ]
-        for _ in fertilizer_types:
+        fertilizer_type_names = (
+            ("Synthetic", "Synthétique"),
+            ("Inoculant", "Inoculant"),
+            ("Organic compost", "Compost organique"),
+            ("Manure", "Fumier"),
+            ("Bone meal", "Farine d'os"),
+            ("Fish emulsion", "Émulsion de poisson"),
+            ("Blood meal", "Farine de sang"),
+            ("Seaweed fertilizer", "Engrais d'algues"),
+            ("Bat guano", "Guano de chauve-souris"),
+            ("Worm castings", "Moulée de vers"),
+            ("Compost tea", "Thé de compost"),
+            ("Wood ash", "Cendre de bois"),
+            ("Rock phosphate", "Phosphate de roche"),
+            ("Greensand", "Sable vert"),
+            ("Alfalfa meal", "Farine d'alfalfa"),
+            ("Cottonseed meal", "Farine de tourteau de coton"),
+            ("Feather meal", "Farine de plumes"),
+            ("Humic acid", "Acide humique"),
+        )
+        for name in fertilizer_type_names:
             Fertilizertype.objects.create(
-                name=Internationalization.objects.create(en=_[0], fr=_[1])
+                name=Internationalization.objects.create(en=name[0], fr=name[1])
             )
 
     def create_mulch_layer_types(self):
-        mulch_layer_types = [
-            ["Sheep wool", "Laine de mouton"],
-            ["Cardboard", "Carton"],
-            ["Compost", "Compost"],
-            ["Woodchips", "Copeaux de bois"],
-            ["Saw dust", "Poussière de scie"],
-            ["Corn husk", "Feuille de maïs"],
-        ]
-        for _ in mulch_layer_types:
+        mulch_layer_type_names = (
+            ("Sheep wool", "Laine de mouton"),
+            ("Cardboard", "Carton"),
+            ("Compost", "Compost"),
+            ("Woodchips", "Copeaux de bois"),
+            ("Saw dust", "Poussière de scie"),
+            ("Corn husk", "Feuille de maïs"),
+        )
+        for name in mulch_layer_type_names:
             Mulchlayertype.objects.create(
-                name=Internationalization.objects.create(en=_[0], fr=_[1])
+                name=Internationalization.objects.create(en=name[0], fr=name[1])
             )
 
     def create_tree_types(self):
-        for _ in tree_types:
-            Treetype.objects.create(name=Internationalization.objects.create(en=_[0], fr=_[1]))
+        for name in tree_type_names:
+            Treetype.objects.create(
+                name=Internationalization.objects.create(en=name[0], fr=name[1])
+            )
 
     def create_site_types(self):
-        site_types = [
-            ["Parks", "Parcs"],
-            ["Indigenous community", "Communauté Indigène"],
-            ["Educational Facility", "Établissement d'enseignement"],
-            ["Farms Land", "Terres agricoles"],
-            ["Corporate Lot", "Lot d'entreprise"],
-        ]
+        site_type_names = (
+            ("Parks", "Parcs"),
+            ("Indigenous community", "Communauté Indigène"),
+            ("Educational Facility", "Établissement d'enseignement"),
+            ("Farms Land", "Terres agricoles"),
+            ("Corporate Lot", "Lot d'entreprise"),
+        )
 
-        for _ in site_types:
-            Sitetype.objects.create(name=Internationalization.objects.create(en=_[0], fr=_[1]))
+        for name in site_type_names:
+            Sitetype.objects.create(
+                name=Internationalization.objects.create(en=name[0], fr=name[1])
+            )
 
     def create_assets(self):
         image_file_names = (
