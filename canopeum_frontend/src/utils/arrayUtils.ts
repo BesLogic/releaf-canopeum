a1c7352f2937957f894f3b02b656b271cfc20c63
Samuel Therrien <samuel.therrien@beslogic.com>
2024-10-29 15:37:56 -0400

    DOn't inline disable total-functions/no-unsafe-type-assertion

--- canopeum_frontend/src/utils/arrayUtils.ts
+++ canopeum_frontend/src/utils/arrayUtils.ts
@@ -27,7 +27,7 @@ export const mapSum = <
   values.reduce(
     (previous, current) =>
       previous + (
-        /* eslint-disable-next-line total-functions/no-unsafe-type-assertion --
+        /* total-functions/no-unsafe-type-assertion --
         The retriction using KeysWithValsOfType ensures the value obtained from the key is of known
         type. We don't want the the objects to be restricted to *only* those value types */
         (current[key] as number | null | undefined) ?? 0
