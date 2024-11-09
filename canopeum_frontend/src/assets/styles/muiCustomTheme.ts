6db3a40b00cb9ae3b474dc1a9f5fabbd8abe4d70
Samuel Therrien <samuel.therrien@beslogic.com>
2024-11-08 22:28:05 -0500

    stop using @import

--- canopeum_frontend/src/assets/styles/muiCustomTheme.ts
+++ canopeum_frontend/src/assets/styles/muiCustomTheme.ts
@@ -3,20 +3,24 @@ import { createTheme } from '@mui/material'
 import variables from './export.module.scss'
 
 const muiCustomTheme = createTheme({
-  palette: {
-    success: {
-      light: variables.success,
-      main: variables.success,
-      dark: variables.success,
-      contrastText: variables.success,
-    },
-    secondary: {
-      light: variables.secondary,
-      main: variables.secondary,
-      dark: variables.secondary,
-      contrastText: variables.secondary,
-    },
-  },
+  // palette: {
+  //   success: {
+  //     light: variables.success,
+  //     main: variables.success,
+  //     dark: variables.success,
+  //     contrastText: variables.success,
+  //   },
+  //   secondary: {
+  //     light: variables.secondary,
+  //     main: variables.secondary,
+  //     dark: variables.secondary,
+  //     contrastText: variables.secondary,
+  //   },
+  //   background: {
+  //     default: variables.cardBg,
+  //     paper: variables.cardBg,
+  //   },
+  // },
 })
 
 export default muiCustomTheme
