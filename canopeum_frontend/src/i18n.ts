5e9af5282cec14427808fa769848c9fc9b40f1b5
Samuel T. <samuel.therrien@beslogic.com>
2024-11-26 15:36:23 -0500

    Automatic language detection and save (#305)

--- canopeum_frontend/src/i18n.ts
+++ canopeum_frontend/src/i18n.ts
@@ -1,9 +1,14 @@
 import { default as i18n } from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
 import resources from './locale'
 
-void i18n.use(initReactI18next).init({
-  resources,
-  lng: 'en',
-})
+void i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    supportedLngs: Object.keys(resources),
+    resources,
+    detection: { convertDetectedLanguage: lng => lng.split('-')[0] }, // fr-CA -> fr
+  })
