fab14cc72b61e1b909d4584984db1fb136ed3662
Gabriel Gilbert <34329520+gab-gil@users.noreply.github.com>
2024-12-17 12:42:35 -0500

    refactor: change register form to react hook form (#332)
    Co-authored-by: gab-gil <gabriel.gilbert@beslogic.com>
    Co-authored-by: Samuel Therrien <samuel.therrien@beslogic.com>

--- canopeum_frontend/src/utils/validators.ts
+++ canopeum_frontend/src/utils/validators.ts
@@ -1,8 +1,8 @@
-const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[\d!#$%&*?@A-Za-z]{8,}$/u
+export const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.{8}).+$/u
 
 export const isValidPassword = (input: string) => new RegExp(passwordRegex).test(input)
 
-const emailRegex = /^[^@]+@[^@][^.@]*\.[^@]+$/u
+export const emailRegex = /^[^@]+@[^@][^.@]*\.[^@]+$/u
 
 export const isValidEmail = (input: string) => new RegExp(emailRegex).test(input)
 
