9bb67d844a017c4a1c41245bf8da88dd94e10cf8
Samuel Therrien <samuel.therrien@beslogic.com>
2025-01-21 16:36:24 -0500

    Migrate to ESLint Flat Config

--- canopeum_frontend/src/services/errors.ts
+++ canopeum_frontend/src/services/errors.ts
@@ -2,5 +2,6 @@ export const ensureError = (error: unknown) => {
   if (error instanceof Error) return error
   if (error === undefined) return new Error(error)
 
+  // eslint-disable-next-line @typescript-eslint/no-base-to-string -- This is a fallback
   return new Error(String(error))
 }
