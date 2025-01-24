89f612b53851059aadc3b5f6ad1c3868223d2398
Samuel T. <samuel.therrien@beslogic.com>
2025-01-24 12:18:10 -0500

    Not using top-level await

--- canopeum_frontend/src/i18n.ts
+++ canopeum_frontend/src/i18n.ts
@@ -4,7 +4,9 @@ import { initReactI18next } from 'react-i18next'
 
 import resources from './locale'
 
-await i18n
+/* eslint-disable-next-line @typescript-eslint/no-floating-promises
+-- Not all browsers we target support top-level await */
+void i18n
   .use(initReactI18next)
   .use(LanguageDetector)
   .init({
