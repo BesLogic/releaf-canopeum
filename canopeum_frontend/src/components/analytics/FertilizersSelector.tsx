e596ab5fefcdd2eef415bdac3dc0bf5971f87150
Olivier B. <93934995+BesOlivierBouchard@users.noreply.github.com>
2025-06-04 19:09:32 -0400

    #244 Unify Multiselect (#364)
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Samuel T. <samuel.therrien@beslogic.com>

--- canopeum_frontend/src/components/analytics/FertilizersSelector.tsx
+++ canopeum_frontend/src/components/analytics/FertilizersSelector.tsx
@@ -1,8 +1,8 @@
 import { useCallback, useContext, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import OptionQuantitySelector, { type SelectorOption, type SelectorOptionQuantity } from '@components/analytics/OptionQuantitySelector'
 import { LanguageContext } from '@components/context/LanguageContext'
+import OptionQuantitySelector, { type SelectorOption, type SelectorOptionQuantity } from '@components/inputs/OptionQuantitySelector'
 import useApiClient from '@hooks/ApiClientHook'
 import useErrorHandling from '@hooks/ErrorHandlingHook'
 import { FertilizerType } from '@services/api'
